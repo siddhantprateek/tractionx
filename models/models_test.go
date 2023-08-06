1068a49ba40b14f30c471083572e5eadf4bc2c00
siddhantprateek <siddhantprateek@gmail.com>
2023-08-06 23:26:11 +0530

    ft(test): chaincode test cases

---
+++ models/models_test.go
@@ -0,0 +1,29 @@
+package models_test
+
+import (
+	"encoding/json"
+	"testing"
+	models "tractionx/models"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPropertyJSONSerialization(t *testing.T) {
+	property := models.Property{
+		ID:           "prop123",
+		PlotNumber:   1,
+		Builder:      "ABC Builders",
+		OwnedBy:      "John Doe",
+		CurrentPrice: 100000,
+		SellingPrice: 120000,
+	}
+
+	propertyJSON, err := json.Marshal(property)
+	assert.NoError(t, err, "JSON marshaling should not return an error")
+
+	var deserializedProperty models.Property
+	err = json.Unmarshal(propertyJSON, &deserializedProperty)
+	assert.NoError(t, err, "JSON unmarshaling should not return an error")
+
+	assert.Equal(t, property, deserializedProperty, "Deserialized Property should be equal to the original Property")
+}
